b463f282c1094234b627549d0fbdaecb0446e0c8
Andezion <245122@edu.p.lodz.pl>
2026-07-10 23:35:36 +0200

    add: base stuff

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window/Keyboard.hpp>
+#include <iostream>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(1200, 800), "Simulator");
+    window.setFramerateLimit(120);
+
+    sf::RectangleShape rectangle(sf::Vector2f(120.f, 50.f));
+    rectangle.setFillColor(sf::Color::Green);
+    rectangle.setPosition(100.f, 100.f);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) || sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
+                window.close();
+            }
+
+            if (event.type == sf::Event::Resized) {
+                std::cout << "new width: " << event.size.width << std::endl;
+                std::cout << "new height: " << event.size.height << std::endl;
+            }
+        }
+
+        window.clear(sf::Color::White);
+        window.draw(rectangle);
+        window.display();
+    }
+}
