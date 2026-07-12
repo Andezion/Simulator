061ee1aee19f38e103cd7b2b29505c770be57895
Andezion <245122@edu.p.lodz.pl>
2026-07-12 19:38:27 +0200

    add: added resizebale components

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/Keyboard.hpp>
-#include <iostream>
+// #include <iostream>
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(1200, 800), "Simulator");
     window.setFramerateLimit(120);
 
-    sf::RectangleShape rectangle(sf::Vector2f(120.f, 50.f));
+    sf::RectangleShape rectangle(sf::Vector2f(window.getSize().x / 10.0f, window.getSize().y / 20.0f));
     rectangle.setFillColor(sf::Color::Green);
     rectangle.setPosition(100.f, 100.f);
 
@@ -17,10 +17,10 @@ int main() {
                 window.close();
             }
 
-            if (event.type == sf::Event::Resized) {
-                std::cout << "new width: " << event.size.width << std::endl;
-                std::cout << "new height: " << event.size.height << std::endl;
-            }
+            // if (event.type == sf::Event::Resized) {
+            //     std::cout << "new width: " << event.size.width << std::endl;
+            //     std::cout << "new height: " << event.size.height << std::endl;
+            // }
         }
 
         window.clear(sf::Color(184, 183, 177, 255));
