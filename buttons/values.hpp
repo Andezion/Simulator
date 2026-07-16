7e27ea87f532a915af78c91df1562868bcb79e41
Andezion <245122@edu.p.lodz.pl>
2026-07-16 14:36:45 +0200

    add: started button work

--- buttons/values.hpp
+++ buttons/values.hpp
@@ -3,6 +3,31 @@
 #include <SFML/Graphics/Vertex.hpp>
 #include <SFML/Window/Keyboard.hpp>
 
-class Values {
+struct RectInfo {
+    float size_x;
+    float size_y;
+    float pos_x;
+    float pos_y;
+};
 
+class Values {
+public:
+    sf::RectangleShape F_value;
+    sf::RectangleShape m_value;
+
+    Values(struct RectInfo F_info, struct RectInfo m_info) {
+        F_value.setSize(sf::Vector2f(F_info.size_x, F_info.size_y));
+        F_value.setFillColor(sf::Color::Red);
+        F_value.setPosition(F_info.pos_x, F_info.pos_y);
+        F_value.setOutlineColor(sf::Color::Black);
+        F_value.setOutlineThickness(1.0f);
+           
+        m_value.setSize(sf::Vector2f(m_info.size_x, m_info.size_y));
+        m_value.setFillColor(sf::Color::Blue);
+        m_value.setPosition(m_info.pos_x, m_info.pos_y);
+        m_value.setOutlineColor(sf::Color::Black);
+        m_value.setOutlineThickness(1.0f);
+    }
+
+    ~Values() = default;
 };
